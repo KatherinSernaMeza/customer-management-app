6123f8a601daf43e0800ec830b33721a10712fbc
Katherin Serna <katherin.serna.m@gmail.com>
2023-07-10 21:52:16 -0700

    test response

--- src/api/services/customers.tsx
+++ src/api/services/customers.tsx
@@ -3,13 +3,8 @@ import axios from "axios";
 
 export const CostumersAPI = {
   get: async function (customers: string) {
-    const response = await axios(customers, {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsInVzZXJuYW1lIjoia21pbmNoZWxsZSIsImVtYWlsIjoia21pbmNoZWxsZUBxcS5jb20iLCJmaXJzdE5hbWUiOiJKZWFubmUiLCJsYXN0TmFtZSI6IkhhbHZvcnNvbiIsImdlbmRlciI6ImZlbWFsZSIsImltYWdlIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvYXV0cXVpYXV0LnBuZz9zaXplPTUweDUwJnNldD1zZXQxIiwiaWF0IjoxNjM1NzczOTYyLCJleHAiOjE2MzU3Nzc1NjJ9.n9PQX8w8ocKo0dMCw3g8bKhjB8Wo7f7IONFBDqfxKhs",
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await axios("https://dummyjson.com/users");
+
     // returning the customers returned by the API
     return response.data;
   },
