ba6de2cd27d6286d251da56cb6be2a5a6d197140
Katherin Serna <katherin.serna.m@gmail.com>
2023-07-20 13:52:46 -0700

    fix overflow main container customers

--- src/api/services/customers.tsx
+++ src/api/services/customers.tsx
@@ -3,7 +3,6 @@ import axios from "axios";
 
 export const CostumersAPI = {
   get: async function (customers: string) {
-    console.log(customers, "customers");
     const response = await axios(customers);
     // returning the customers returned by the API
     return response.data;
