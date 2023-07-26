0ae89f3d2fbffa7df31689be8baf3de4ebc1bb85
Katherin Serna <katherin.serna.m@gmail.com>
2023-07-25 19:42:43 -0700

    device customers

--- src/components/customers/CustomersDevice.tsx
+++ src/components/customers/CustomersDevice.tsx
@@ -26,37 +26,45 @@ const CustomersDevice = () => {
   }, []);
   return (
     <div>
-      {customers.map((customer) => (
-        <div key={customer["id"]}>
-          <div className="display-between display-bottom">
-            <p className="title-name ">
-              {customer["id"]} &nbsp;&nbsp; {customer["firstName"]}{" "}
-              {customer["lastName"]}
-            </p>
-            <span>
-              <FaEdit size={15} className="faplus" />
-              <FaTrash size={15} className="faplus" />
-            </span>
-          </div>
-          <p className="title">Age</p>
-          <p>{customer["age"]}</p>
-          <p className="title">Gender</p>
-          <p>{customer["gender"]}</p>
-          <p className="title">Email</p>
-          <p>{customer["email"]}</p>
-          <p className="title">Phone</p>
-          <p>{customer["phone"]}</p>
-          <p className="title">Username</p>
-          <p>{customer["username"]}</p>
-          <p className="title">Password</p>
-          <p>{customer["password"]}</p>
-          <p className="title">BirthDate</p>
-          <p>{customer["birthDate"]}</p>
-          <p className="title">Image</p>
-          <p>{customer["image"]}</p>
-          <hr />
-        </div>
-      ))}
+      {loading ? (
+        <h1> its loading... </h1>
+      ) : (
+        <>
+          {customers.map((customer) => (
+            <div key={customer["id"]}>
+              <div className="display-between display-bottom">
+                <p className="title-name ">
+                  {customer["id"]} &nbsp;&nbsp; {customer["firstName"]}{" "}
+                  {customer["lastName"]}
+                </p>
+                <span>
+                  <FaEdit size={15} className="faplus" />
+                  <FaTrash size={15} className="faplus" />
+                </span>
+              </div>
+              <p className="title">Age</p>
+              <p>{customer["age"]}</p>
+              <p className="title">Gender</p>
+              <p>{customer["gender"]}</p>
+              <p className="title">Email</p>
+              <p>{customer["email"]}</p>
+              <p className="title">Phone</p>
+              <p>{customer["phone"]}</p>
+              <p className="title">Username</p>
+              <p>{customer["username"]}</p>
+              <p className="title">Password</p>
+              <p>{customer["password"]}</p>
+              <p className="title">BirthDate</p>
+              <p>{customer["birthDate"]}</p>
+              <p className="title">Image</p>
+              <p>{customer["image"]}</p>
+              <hr />
+            </div>
+          ))}
+        </>
+      )}
+
+      {error ? "its error..." : ""}
     </div>
   );
 };
